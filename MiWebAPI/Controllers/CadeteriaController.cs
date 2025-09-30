1c6ff41c75399a278b8e796ad341e7f4640373ff
Juan Martin Feliu <jmfeliu04@gmail.com>
2025-09-30 19:55:29 -0300

    TP terminado

--- MiWebAPI/Controllers/CadeteriaController.cs
+++ MiWebAPI/Controllers/CadeteriaController.cs
@@ -31,26 +31,48 @@ public class CadeteriaControler: ControllerBase{
     [HttpGet("informe")]
     public IActionResult GetInforme()
     {
-        return Ok(cadeteria.GenerarInforme());    
+        var cadeteria = new Cadeteria("Pedido Ya", "381596486");
+
+        // Si querés, podés cargar los pedidos desde JSON
+        var ADPedidos = new AccesoADatosPedidos();
+        var pedidos = ADPedidos.Obtener();
+        foreach (var pedido in pedidos)
+        {
+            cadeteria.AgregarPedido(pedido);
+        }
+
+        return Ok(cadeteria.GenerarInforme());
     }
+
 
     [HttpPost("AgregarPedido")]
     public IActionResult AgregarPedido(Pedidos pedido)
     {
-        var accesoPedidos = new AccesoADatosPedidos();
-        var PedidoCreado = accesoPedidos.Guardar(pedido);
+        var cadeteria = new Cadeteria("Pedido Ya", "381596486");
+        cadeteria.AgregarPedido(pedido);
+
+        var ADPedidos = new AccesoADatosPedidos();
+        ADPedidos.Guardar(cadeteria.ListadoPedidos);
+
+        return Ok("Pedido Agregado");
     }
 
     [HttpPut]
     public IActionResult AsignarPedido(int idPedido, int idCadete)
     {
+        var cadeteria = new Cadeteria("Pedido Ya", "381596486");
         cadeteria.AsignarCadeteAPedido(idCadete, idPedido);
-        return Ok("Pedido Asignado");    
+
+        var ADPedidos = new AccesoADatosPedidos();
+        ADPedidos.Guardar(cadeteria.ListadoPedidos);
+
+        return Ok("Pedido Asignado");
     }
 
     [HttpPut("CambiarEstado")]
     public IActionResult CambiarEstadoPedido(int idPedido, string NuevoEstado)
     {
+        var cadeteria = new Cadeteria("Pedido Ya", "381596486");
         var pedido = cadeteria.ListadoPedidos.FirstOrDefault(p => p.Numero == idPedido);
         if (pedido == null)
         {
@@ -63,6 +85,7 @@ public class CadeteriaControler: ControllerBase{
     [HttpPut("CambiarCadete")]
     public IActionResult CambiarCadetePedido(int idPedido, int idNuevoCadete)
     {
+        var cadeteria = new Cadeteria("Pedido Ya", "381596486");
         cadeteria.AsignarCadeteAPedido(idNuevoCadete, idPedido);
         return Ok("Cadete cambiado");
     }
